1f9b03bfdfe41d21ac74e80d449cbe2712244fc8
LZMF <121703585+oslini23@users.noreply.github.com>
2025-06-07 13:21:21 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Bem-vindo ao S-Sir Dober!</h1>
+      <p>Este site foi publicado com GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
